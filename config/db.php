146f1dec65d767d2ef5c99553358919af4422e35
TENDREL TSHERING <tendreltshering758@gmail.com>
2025-03-19 13:02:36 +0800

    wip: Commence Framework
    - Create the db.php config file
    - Create helper.php file with functions

---
+++ config/db.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Database Configuration File
+ *
+ * Filename:        db.php
+ * Location:        config/
+ * Project:         XXX-SaaS-Vanilla-MVC-SN
+ * Date Created:    DATE_CREATED
+ *
+ * Author:          YOUR NAME
+ *
+ */
+return [
+    'host' => '127.0.0.1',
+    'port' => 3306,
+    'dbname' => 'TOT_SaaS_FED_2025_S1',
+    'username' => 'TOT_SaaS_FED_2025_S1',
+    'password' => 'PasswordSecret'
+];
